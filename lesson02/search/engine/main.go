4be3a891f0d18467be46547af3cbebe20ded9566
Адамцов Артем Юрьевич <AYAdamtsov@sberbank.ru>
2020-10-19 09:56:22 +0300

    Code review fix

--- lesson02/search/engine/main.go
+++ lesson02/search/engine/main.go
@@ -18,40 +18,40 @@ func main() {
 		fmt.Printf("ошибка при сканировании сайта %s: %v\n", url, err)
 	}
 
-	var array []page
+	var pages []page
 	for url, title := range data {
 		p := page{url: url, title: title}
-		array = append(array, p)
+		pages = append(pages, p)
 
 		fmt.Printf("Страница %s имеет адрес: %s\n", url, title)
 	}
 
 	// w := flag.String("w", "", "Target word")
 	// flag.Parse()
-	// word := strings.ToLower(*w) // Обязательная часть
+	// w = strings.ToLower(*w) // Обязательная часть
 
 	for {
-		fmt.Printf("Enter word or quit for exit: ")
+		fmt.Print("Enter word or quit for exit: ")
 
 		var w string
 		fmt.Scanln(&w)
-		word := strings.ToLower(w)
+		w = strings.ToLower(w)
 
-		if word == "quit" {
-			fmt.Printf("Exit!\n")
+		if w == "quit" {
+			fmt.Println("Exit!")
 			return
 		}
 
-		for _, p := range array {
+		for _, p := range pages {
 			url := strings.ToLower(p.url)
 			title := strings.ToLower(p.title)
 
-			if strings.Contains(url, word) || strings.Contains(title, word) {
+			if strings.Contains(url, w) || strings.Contains(title, w) {
 				fmt.Printf("%s - %s\n", p.url, p.title)
 			}
 		}
 
-		fmt.Printf("\n")
+		fmt.Print("\n")
 	}
 
 }
